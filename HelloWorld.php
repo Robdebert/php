31560e045d16ff77866eab0d3740f5a2bf67c617
Robert Heine <bitbucket@deltatech-v2.de>
2016-06-25 22:19:43 +0200

    PHP Unit 5 Tests, erfolgreich

--- HelloWorld.php
+++ HelloWorld.php
@@ -13,24 +13,31 @@ class HelloWorld
     }
     
     public function createDBIFNotExists() {
-        $sql = "CREATE TABLE IF NOT EXISTS hello;";
-        $this->pdo->query($sql);
-        return true;
+        $sql = "CREATE DATABASE IF NOT EXISTS hello;";
+        $result = $this->pdo->query($sql);
+        return (bool) $result;
     }
-    
-
-    public function hello($what = 'World')
-    {
-        $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
-        $this->pdo->query($sql);
-        return "Hello $what";
+    public function createTableIFNotExists() {
+        $sql = "CREATE TABLE IF NOT EXISTS `world` ( `what` VARCHAR(255) NOT NULL , PRIMARY KEY (`what`)) ENGINE = InnoDB;";
+        $result = $this->pdo->prepare($sql)->execute();
+        return (bool) $result;
     }
 
-
-    public function what()
+    public function insert($message)
     {
-        $sql = "SELECT what FROM hello";
-        $stmt = $this->pdo->query($sql);
-        return $stmt->fetchColumn();
+        $sql = "INSERT INTO world VALUES ('{$message}')";
+		$sth = $this->pdo->prepare($sql);
+        $result = $sth->execute();
+        return (bool) $result;
+    }
+
+
+    public function getAll()
+    {
+        $sql = "SELECT * FROM world";
+        $sth = $this->pdo->prepare($sql);
+		$sth->execute();
+		$result_array = $sth->fetchAll(PDO::FETCH_ASSOC);
+        return $result_array;
     }
 }
